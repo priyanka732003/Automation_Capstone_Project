d616ff78438d39a73f3b0721426f1057491e89ff
priyanka732003 <priyankabayyapuneni474@gmail.com>
2025-09-08 11:52:32 +0530

    Add files via upload

---
+++ saucedemo_By using BDD-Cucumber/src/test/java/runners/TestRunner.java
@@ -0,0 +1,13 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/feature",
+        glue = {"stepdefs"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
